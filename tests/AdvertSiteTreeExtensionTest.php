73f3810d3363b3d3ada34697cc630e341269558b
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-02-29 23:43:40 +0700

    WIP: Test of getting category with a child

--- tests/AdvertSiteTreeExtensionTest.php
+++ tests/AdvertSiteTreeExtensionTest.php
@@ -2,4 +2,15 @@
 
 class AdvertSiteTreeExtensionTest extends SapphireTest
 {
+    public static $fixture_file = 'adverts/tests/adverts.yml';
+
+    /*
+    Test child of a parent with a set advert category ID
+     */
+    public function testCalculateAdvertCategoryIDChild() {
+        $sectionPage = $this->objFromFixture('Page', 'Section');
+        $page = $this->objFromFixture('Page', 'SectionChild');
+        $categoryID = $page->CalculateAdvertCategoryID();
+        $this->assertEquals($sectionPage->AdvertCategoryID, $categoryID);
+    }
 }
